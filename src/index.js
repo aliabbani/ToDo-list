fb6c953df0de8af0100563d8de2e649a930e825d
ali abbani <aliox@ahlam.com>
2021-08-17 20:52:16 +0300

    fix linters error

--- src/index.js
+++ src/index.js
@@ -3,40 +3,40 @@ import "./style.css";
 
 let tasks = [
   {
-    'description': 'Adding a new item',
-    'completed': false,
-    'index': 0,
+    description: "Adding a new item",
+    completed: false,
+    index: 0,
   },
   {
-    'description': 'Removing a selected item.',
-    'completed': false,
-    'index': 1,
+    description: "Removing a selected item.",
+    completed: false,
+    index: 1,
   },
   {
-    'description': "Reordering a selected item (as drag-and-drop).",
-    'completed': false,
-    'index': 2,
+    description: "Reordering a selected item (as drag-and-drop).",
+    completed: false,
+    index: 2,
   },
   {
-    'description': "Marking a selected item as complete.",
-    'completed': false,
-    'index': 3,
+    description: "Marking a selected item as complete.",
+    completed: false,
+    index: 3,
   },
   {
-    'description': "Removing all items marked as complete at once.",
-    'completed': false,
-    'index': 4,
+    description: "Removing all items marked as complete at once.",
+    completed: false,
+    index: 4,
   },
 ];
 
-for (let i = 0; i < tasks.length; i++) {
-  const listItems = document.createElement('div');
-  const leftItems = document.createElement('ul');
-  const liTextAndIcons = document.createElement('li');
-  const squareSpan = document.createElement('span')
-  const squareIcon = document.createElement('i');
-  const listText = document.createElement('p');
-  const threeDotIcon = document.createElement('i');
+for (let i = 0; i < tasks.length; i += 1) {
+  const listItems = document.createElement("div");
+  const leftItems = document.createElement("ul");
+  const liTextAndIcons = document.createElement("li");
+  const squareSpan = document.createElement("span");
+  const squareIcon = document.createElement("i");
+  const listText = document.createElement("p");
+  const threeDotIcon = document.createElement("i");
 
   listItems.appendChild(leftItems);
   listItems.appendChild(threeDotIcon);
@@ -45,14 +45,15 @@ for (let i = 0; i < tasks.length; i++) {
   liTextAndIcons.appendChild(listText);
   squareSpan.appendChild(squareIcon);
 
-  listItems.className = 'list-items';
-  leftItems.className = 'fa-ul left-items';
-  liTextAndIcons.className = 'li-text-and-icons';
-  squareSpan.className = 'fa-li square-span';
-  squareIcon.className = 'far fa-square square-icon';
-  listText.className = 'list-text';
-  threeDotIcon.className = 'fas fa-ellipsis-v three-dot-icon';
+  listItems.className = "list-items";
+  leftItems.className = "fa-ul left-items";
+  liTextAndIcons.className = "li-text-and-icons";
+  squareSpan.className = "fa-li square-span";
+  squareIcon.className = "far fa-square square-icon";
+  listText.className = "list-text";
+  threeDotIcon.className = "fas fa-ellipsis-v three-dot-icon";
 
-  document.querySelector('.box').appendChild(listItems);
+  document.querySelector(".box").appendChild(listItems);
   listText.innerText = tasks[i].description;
+  listText.innerHTML;
 }
