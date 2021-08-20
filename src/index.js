34b9f402b9862cb900c202bddd859a3c43ab627e
ali abbani <aliox@ahlam.com>
2021-08-20 14:36:03 +0300

    Implement a function for adding a new task

--- src/index.js
+++ src/index.js
@@ -18,14 +18,16 @@ const getData = () => {
 getData();
 
 const orderTasks = () => {
-  let max = tasks[0].index;
-  for (let j = 1; j < tasks.length; j += 1) {
-    if (tasks[j].index > max) {
-      max = tasks[j].index;
-    } else {
-      const ali = tasks[j];
-      tasks[j] = tasks[j - 1];
-      tasks[j - 1] = ali;
+  if (tasks.length > 2) {
+    let max = tasks[0].index;
+    for (let j = 1; j < tasks.length; j += 1) {
+      if (tasks[j].index > max) {
+        max = tasks[j].index;
+      } else {
+        const ali = tasks[j];
+        tasks[j] = tasks[j - 1];
+        tasks[j - 1] = ali;
+      }
     }
   }
 };
@@ -75,21 +77,60 @@ generateTasks();
 
 // start addRemove js
 
-const addNewTask = (tasks) => {
+const addNewTask = () => {
   const inputAdd = document.querySelector('#tasks-text').value;
-  const listText = document.createElement('p');
-  // listText.innerHTML = inputAdd.value;
-  const addT = {
-    inputAdd,
+  let newIndex;
+  if (tasks === null) {
+    newIndex = 1;
+  } else {
+    newIndex = tasks.length + 1;
+  }
+  const task = {
+    description: inputAdd,
+    index: newIndex,
+    completed: false,
   };
-  tasks.push(addT);
+  tasks.push(task);
   localStorage.setItem('tasks', JSON.stringify(tasks));
-  generateTasks();
+  return task;
 };
+const generateOneTask = (task) => {
+  const div1 = document.createElement('div');
+  const div2 = document.createElement('div');
+  const squareIcon = document.createElement('input');
+  squareIcon.type = 'checkbox';
+  const listText = document.createElement('p');
+  listText.type = 'text';
+  const threeDotIcon = document.createElement('i');
+  div1.appendChild(div2);
+  div1.appendChild(threeDotIcon);
+  div2.appendChild(squareIcon);
+  div2.appendChild(listText);
 
+  div1.className = 'list-items';
+  div2.className = 'left-items';
+  listText.className = 'list-text1';
+  squareIcon.className = 'checkbox';
+  threeDotIcon.className = 'fas fa-ellipsis-v three-dot-icon';
+  document.querySelector('.box').appendChild(div1);
+  listText.innerText = task.description;
+
+  if (task.completed === true) {
+    listText.classList.add('list-text');
+    squareIcon.checked = true;
+  } else {
+    listText.classList.remove('list-text');
+    squareIcon.checked = false;
+  }
+  squareIcon.addEventListener('change', (event) => {
+    updateCompleted(event, task, listText);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  });
+};
 const addIcon = document.querySelector('.add-here');
 addIcon.addEventListener('click', () => {
-  addNewTask(tasks);
+  addIcon.style.color = 'blue';
+  const getTask = addNewTask();
   localStorage.setItem('tasks', JSON.stringify(tasks));
-  generateTasks();
+  generateOneTask(getTask);
 });
